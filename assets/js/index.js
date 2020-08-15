05b1ff7aaffb47575524404d8965d62081305bba
matt-pearson29 <matt.pearson29@googlemail.com>
2020-08-15 16:48:03 +0000

    java scriptfor the on button function written

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,42 +10,39 @@ let strict = false;
 let on = false;
 let win;
 
-const turnCounter = document.querySelector("#count");
-const btn1 = document.querySelector("#btn1");
-const btn2 = document.querySelector("#btn2");
-const btn3 = document.querySelector("#btn3");
-const btn4 = document.querySelector("#btn4");
-const btn5 = document.querySelector("#btn5");
-const btn6 = document.querySelector("#btn6");
-const btn7 = document.querySelector("#btn7");
-const btn8 = document.querySelector("#btn8");
-const strictButton = document.querySelector("#strict");
-const onButton = document.querySelector("#on");
-const startButton = document.querySelector("#startbtn");
+const turnCounter =("count");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+const btn6 = document.getElementById ("btn6");
+const btn7 = document.getElementById("btn7");
+const btn8 = document.getElementById("btn8");
+const strictButton = document.getElementById("strict");
+const onButton =("on");
+const startButton = document.getElementById("startbtn");
 
 //displays the rules of the game in a alert box//
 function rulesFunction() {
    alert("HOW TO PLAY\n Check the on/off box to power on\n Click the start button to start the game\n The level counter will display 01\n A random sequence of colors will flash\n Repeat the sequence by clicking on the colors\n If the sequence is correct then the level will increase by one\n In default mode if the wrong sequence\n is entered the level will go back\n to the previous correct level\n if in strict mode the game will start from the beginning."); 
-} 
+}; 
+
+function turnon() {
+  if (onButton.checked ==true){ 
+    on = true;
+    document.getElementById(count).innerHTML = "00";
+}else {
+      on == false
+      count.innerHTML = "";
+  }
+  {console.log("checked")};
+};
 
 //Turns the game on and off.//
-on.addEventListener('click',() => {
-  if (on.checked == true) {
-    on = true;
-    count.innerHTML = 0;
-  } else {
-    on = false;
-    count.innerHTML = "";
-  }
-});
+
 //selects default or strict mode//
-strictButton.addEventListener('click', (_event) => {
-  if (strictButton.checked == true) {
-    strict = true;
-  } else {
-    strict = false;
-  }
-});
+
 
 
 
