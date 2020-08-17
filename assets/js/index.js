d31a39896596865ae35e65371a0c4a204874864c
matt-pearson29 <matt.pearson29@googlemail.com>
2020-08-17 13:14:49 +0000

    completed the java script and jquery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,236 +1,371 @@
 //Variables setup for the app//
-let order = [];
-let playerOrder = [];
-let flash;
-let turn;
-let good;
-let compTurn;
-let intervalId;
-let strict = false;
-let on = false;
-let win;
-let level= 0;
-
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
-const btn5 = document.getElementById("btn5");
-const btn6 = document.getElementById ("btn6");
-const btn7 = document.getElementById("btn7");
-const btn8 = document.getElementById("btn8");
-const strictButton = ("strict");
-const onButton =("on");
-const startButton =("startbtn");
-
+    var random = [];
+    var litID = [];
+    var lit;
+    var clicked = [];
+    var i;
+    var j=0;
+    var count = 1;
+    var on;
+    var off;
+    var strict;
+    var x;
+   
+    
+    
 //displays the rules of the game in a alert box//
 function rulesFunction() {
-   alert("HOW TO PLAY\n Check the on/off box to power on\n Click the start button to start the game\n The level counter will display 01\n A random sequence of colors will flash\n Repeat the sequence by clicking on the colors\n If the sequence is correct then the level will increase by one\n In default mode if the wrong sequence\n is entered the level will go back\n to the previous correct level\n if in strict mode the game will start from the beginning."); 
-}; 
-
-//Turns the game on and off.//
-function turnon() {
-  if (onButton.checked ==true){ 
-    on = true
-    document.getElementById("level")
-    .innerHTML ="0";
-}else {
-      on = false 
-      level.innerHTML =" ";
-      
-  }
-  {console.log("checked")};
+   alert("HOW TO PLAY\n Click the on or off buttons to power on or off the game.\n Click the start button to start the game\n The level counter will display 01\n A random sequence of colors will flash\n Repeat the sequence by clicking on the colors\n If the sequence is correct then the level will increase by one\n In default mode if the wrong sequence\n is entered the level will go back\n to the previous correct level\n if in strict mode the game will start from the beginning."); 
 };
 
+function change1() {
+
+        //set interval length
+        if (count <= 10) {
+            off = 400;
+            on = 800;
+
+        }
+
+        else {
+            off = 250;
+            on = 500;
+        }
+
+               x = setInterval(function () {
+
+                if (random[j] == 1) {
+
+                    lit = 'light1';
+                    $('#btn1').addClass(lit);
+                    
+                    litID.push(1);
+
+
+                    setTimeout(function () {
+
+                        $('#btn1').removeClass(lit);
+
+                    }, off);
+
+                }
+
+                else if (random[j] == 2) {
+
+                    lit = 'light2';
+                    $('#btn2').addClass(lit);
+                    
+                    litID.push(2);
+
+
+                    setTimeout(function () {
+
+                        $('#btn2').removeClass(lit);
+
+                    }, off);
+
+                }
+
+                else if (random[j] == 3) {
+
+                    lit = 'light3';
+                    $('#btn3').addClass(lit);
+                    
+                    litID.push(3)
+
+
+                    setTimeout(function () {
+
+                        $('#btn3').removeClass(lit);
+
+                    }, off);
+
+                }
+
+                else if (random[j] == 4) {
+
+                    lit = 'light4';
+                    $('#btn4').addClass(lit);
+                    
+                    litID.push(4);
+
+
+                    setTimeout(function () {
+
+                        $('#btn4').removeClass(lit);
+
+                    }, off);
+
+                }
+                else if (random[j] == 5) {
+
+                    lit = 'light5';
+                    $('#btn5').addClass(lit);
+                
+                    litID.push(5);
+
+
+                    setTimeout(function () {
+
+                        $('#btn5').removeClass(lit);
+
+                    }, off);
+
+                }
+                else if (random[j] == 6) {
+
+                    lit = 'light6';
+                    $('#btn6').addClass(lit);
+                    
+                    litID.push(6);
+
+
+                    setTimeout(function () {
+
+                        $('#btn6').removeClass(lit);
+
+                    }, off);
+
+                }
+                else if (random[j] == 7) {
+
+                    lit = 'light7';
+                    $('#btn7').addClass(lit);
+                    
+                    litID.push(7);
+
+
+                    setTimeout(function () {
+
+                        $('#btn7').removeClass(lit);
+
+                    }, off);
+
+                }
+                else  {
+
+                    lit = 'light8';
+                    
+                    litID.push(8);
+
+
+                    setTimeout(function () {
+
+                        $('#btn8').removeClass(lit);
+
+                    }, off);
+                }
+
+                 j++;
+
+                   if (j>=count) {
+                       clearInterval(x);
+                   }
+
+            }, on);
+
+    }
+
+
+    function checking() {
+
+        if (litID.length == clicked.length) {
+
+                if (litID.join() == clicked.join()) {
+
+                        if (count == 30) {
+
+                            setTimeout(function () {
+                                alert("CONGRATULATIONS You have WON!");
+                                location.reload();
+                            }, 1000);
+                        }
+
+                        else {
+                            setTimeout(function () {
+                                $('#count').text(count + 1);
+                                count++;
+                                litID = [];
+                                clicked = [];
+                                j = 0;
+                                change1();
+
+                            }, 1000);
+                        }
+                }
+
+                else {
+                    if (strict == 1) {
+
+                        location.reload();
+
+                    }
+
+                    else {
+                        setTimeout(function () {
+                            $('#count').text('!!');
+
+                            litID = [];
+                            clicked = [];
+                            j = 0;
+                            change1();
+                        }, 1000);
+                    }
+                }
+    }
+  }
+
+  //Turns the game on.//
+$('#on').on('click', function () {
+        $('#count').text('0');
+
+        for (i=0; i<30; i++) {
+        random[i] = Math.ceil((Math.random() * 8));
+        }
 //selects default or strict mode//
-function strictM() {
-    if (strictButton.checked ==true){
-        strict = true;
-    } else{
-        strict = false;
-    }
-    {console.log("strict mode on")};
-};
+        $('#strict').on('click', function () {
+            strict=1;
+        
+        });
+
+//turns the game off//
+$('#off').on('click', function () {
+        location.reload();
+    });
+
+//starts the game//
+$('#startbtn').on('click', function () {
+
+            $('#count').text('1');
+
+            change1();
+
+        });
+
+//player to play game//
+$('#btn1').on('click', function () {
+
+            $('#btn1').addClass('light1');
+            
+
+            clicked.push(1);
+
+            setTimeout(function () {
+                $('#btn1').removeClass('light1');
+
+            }, 250);
+
+            checking();
+
+        });
+
+        $('#btn2').on('click', function () {
+
+            $('#btn2').addClass('light2');
+            
+
+            clicked.push(2);
+
+            setTimeout(function () {
+                $('#btn2').removeClass('light2');
+
+            }, 250);
+
+            checking();
+        });
+
+        $('#btn3').on('click', function () {
+
+            $('#btn3').addClass('light3');
+            
+
+            clicked.push(3);
+
+            setTimeout(function () {
+                $('#btn3').removeClass('light3');
+
+            }, 250);
+
+            checking();
+        });
+
+        $('#btn4').on('click', function () {
+
+            $('#btn4').addClass('light4');
+            
+
+            clicked.push(4);
+
+            setTimeout(function () {
+                $('#btn4').removeClass('light4');
+
+            }, 250);
+
+            checking();
+        });
+
+        $('#btn5').on('click', function () {
+
+            $('#btn5').addClass('light5');
+            
+            clicked.push(5);
+
+            setTimeout(function () {
+                $('#btn5').removeClass('light5');
+
+            }, 250);
+
+            checking();
+        });
+
+        $('#btn6').on('click', function () {
+
+            $('#btn6').addClass('light6');
+            
+
+            clicked.push(6);
+
+            setTimeout(function () {
+                $('#btn6').removeClass('light6');
+
+            }, 250);
+
+            checking();
+        });
+
+        $('#btn7').on('click', function () {
+
+            $('#btn7').addClass('light7');
+            
+
+            clicked.push(7);
+
+            setTimeout(function () {
+                $('#btn7').removeClass('light7');
+
+            }, 250);
+
+            checking();
+        });
+
+        $('#btn8').on('click', function () {
+
+            $('#btn8').addClass('light8');
+            
+
+            clicked.push(8);
+
+            setTimeout(function () {
+                $('#btn8').removeClass('light8');
+
+            }, 250);
+
+            checking();
+        });
+
+    });
 
 
-function startButtonFunction() {
-  if (on || win) {
-    play();
-  }
-  {console.log("button has been pressed")};
-};
-
-function play() {
-  win = false;
-  order = [];
-  playerOrder = [];
-  flash = 0;
-  intervalId = 0;
-  turn = 1;
-  turnCounter.innerHTML = 1;
-  good = true;
-  for (var i = 0; i < 20; i++) {
-    order.push(Math.floor(Math.random() * 4) + 1);
-  }
-  compTurn = true;
-
-  intervalId = setInterval(gameTurn, 800);
-}
-
-function gameTurn() {
-  on = false;
-
-  if (flash == turn) {
-    clearInterval(intervalId);
-    compTurn = false;
-    clearColor();
-    on = true;
-  }
-
-  if (compTurn) {
-    clearColor();
-    setTimeout(() => {
-      if (order[flash] == 1) (one);
-      if (order[flash] == 2) (two);
-      if (order[flash] == 3) (three);
-      if (order[flash] == 4) (four);
-      flash++;
-    }, 200);
-  }
-}
-
-function one() {
-  
-  btn1.style.backgroundColor = "lightgreen";
-};
-
-function two() {
-  
-  topRight.style.backgroundColor = "tomato";
-};
-
-function three() {
-  
-  bottomLeft.style.backgroundColor = "yellow";
-};
-
-function four() {
-  
-  bottomRight.style.backgroundColor = "lightskyblue";
-};
-
-function clearColor() {
-  btn1.style.backgroundColor = "darkgreen";
-  topRight.style.backgroundColor = "darkred";
-  bottomLeft.style.backgroundColor = "goldenrod";
-  bottomRight.style.backgroundColor = "darkblue";
-}
-
-function flashColor() {
-  btn1.style.backgroundColor = "lightgreen";
-  topRight.style.backgroundColor = "tomato";
-  bottomLeft.style.backgroundColor = "yellow";
-  bottomRight.style.backgroundColor = "lightskyblue";
-}
-
-btn1.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(1);
-    check();
-    one();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
-  }
-})
-
-topRight.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(2);
-    check();
-    two();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
-  }
-})
-
-bottomLeft.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(3);
-    check();
-    three();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
-  }
-})
-
-bottomRight.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(4);
-    check();
-    four();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
-  }
-})
-
-function check() {
-  if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1])
-    good = false;
-
-  if (playerOrder.length == 3 && good) {
-    winGame();
-  }
-
-  if (good == false) {
-    flashColor();
-    turnCounter.innerHTML = "NO!";
-    setTimeout(() => {
-      turnCounter.innerHTML = turn;
-      clearColor();
-
-      if (strict) {
-        play();
-      } else {
-        compTurn = true;
-        flash = 0;
-        playerOrder = [];
-        good = true;
-        intervalId = setInterval(gameTurn, 800);
-      }
-    }, 800);
-
-    noise = false;
-  }
-
-  if (turn == playerOrder.length && good && !win) {
-    turn++;
-    playerOrder = [];
-    compTurn = true;
-    flash = 0;
-    turnCounter.innerHTML = turn;
-    intervalId = setInterval(gameTurn, 800);
-  }
-
-}
-
-function winGame() {
-  flashColor();
-  turnCounter.innerHTML = "WIN!";
-  on = false;
-  win = true;
-}
 
 
 
